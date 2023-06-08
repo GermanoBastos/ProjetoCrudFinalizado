979740150db4b346d547ebc613ebcce6d540cc5d
Germano Bastos <germano.silveira.neto@gmail.com>
2023-06-08 16:50:12 -0300

    Aqui funcionou apenas o add

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,22 @@
+import "firebase/firestore";
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Form, Button, Table, Modal } from "react-bootstrap";
+// Inicialize o Firebase
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDz3T-W25Ygmu2M7eBLmnndaqCNHC6uuqU",
+  authDomain: "devops-eaf3d.firebaseapp.com",
+  projectId: "devops-eaf3d",
+  storageBucket: "devops-eaf3d.appspot.com",
+  messagingSenderId: "949173003157",
+  appId: "1:949173003157:web:8a01747c1bb5b7b120cc9c"
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
 
 const App = () => {
   const [formData, setFormData] = useState({
@@ -49,19 +65,25 @@ const App = () => {
 
   const handleAddUser = () => {
     const newUser = { ...formData };
-    setUsers([...users, newUser]);
-    setFormData({
-      name: "",
-      email: "",
-      phone: "",
-      cep: "",
-      address: "",
-      state: "",
-      city: "",
-      neighborhood: "",
-      registrationDate: ""
-    });
-    setShowModal(false);
+    db.collection("users").add(newUser)
+      .then(() => {
+        setUsers([...users, newUser]);
+        setFormData({
+          name: "",
+          email: "",
+          phone: "",
+          cep: "",
+          address: "",
+          state: "",
+          city: "",
+          neighborhood: "",
+          registrationDate: ""
+        });
+        setShowModal(false);
+      })
+      .catch((error) => {
+        console.error("Erro ao adicionar usuário:", error);
+      });
   };
 
   const handleEditUser = () => {
@@ -109,33 +131,33 @@ const App = () => {
     const selectedState = estados.find((estado) => estado.sigla === stateCode);
 
     if (selectedState) {
-      const fetchCities = async () => {
-        const response = await axios.get(
-          `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedState.id}/municipios`
-        );
+      const fetchCidades = async () => {
+        const response = await axios.get(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedState.id}/municipios`);
         setCidades(response.data);
       };
 
-      fetchCities();
+      fetchCidades();
+    } else {
+      setCidades([]);
     }
 
-    setFormData({ ...formData, state: stateCode, city: "" });
+    handleInputChange(e);
   };
 
   return (
-    <div className="container" style={{ marginTop: "20px" }}>
+    <div className="container">
+      <h1>Cadastro de Usuários</h1>
       <Button variant="primary" onClick={() => setShowModal(true)}>
         Novo Usuário
       </Button>
+
       <Modal show={showModal} onHide={() => setShowModal(false)}>
         <Modal.Header closeButton>
-          <Modal.Title>
-            {editIndex === -1 ? "Cadastro de Usuário" : "Editar Usuário"}
-          </Modal.Title>
+          <Modal.Title>{editIndex === -1 ? "Novo Usuário" : "Editar Usuário"}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <Form onSubmit={handleSubmit}>
-            <Form.Group className="mb-3">
+            <Form.Group controlId="name">
               <Form.Label>Nome</Form.Label>
               <Form.Control
                 type="text"
@@ -145,8 +167,8 @@ const App = () => {
                 required
               />
             </Form.Group>
-            <Form.Group className="mb-3">
-              <Form.Label>Email</Form.Label>
+            <Form.Group controlId="email">
+              <Form.Label>E-mail</Form.Label>
               <Form.Control
                 type="email"
                 name="email"
@@ -155,32 +177,28 @@ const App = () => {
                 required
               />
             </Form.Group>
-            <Form.Group className="mb-3">
+            <Form.Group controlId="phone">
               <Form.Label>Telefone</Form.Label>
               <Form.Control
-                type="tel"
+                type="text"
                 name="phone"
                 value={formData.phone}
                 onChange={handleInputChange}
                 required
               />
             </Form.Group>
-            <Form.Group className="mb-3">
+            <Form.Group controlId="cep">
               <Form.Label>CEP</Form.Label>
-              <div className="d-flex align-items-center">
-                <Form.Control
-                  type="text"
-                  name="cep"
-                  value={formData.cep}
-                  onChange={handleInputChange}
-                  required
-                />
-                <Button variant="primary" onClick={handleCepSearch}>
-                  Buscar
-                </Button>
-              </div>
+              <Form.Control
+                type="text"
+                name="cep"
+                value={formData.cep}
+                onChange={handleInputChange}
+                onBlur={handleCepSearch}
+                required
+              />
             </Form.Group>
-            <Form.Group className="mb-3">
+            <Form.Group controlId="address">
               <Form.Label>Endereço</Form.Label>
               <Form.Control
                 type="text"
@@ -190,9 +208,10 @@ const App = () => {
                 required
               />
             </Form.Group>
-            <Form.Group className="mb-3">
+            <Form.Group controlId="state">
               <Form.Label>Estado</Form.Label>
-              <Form.Select
+              <Form.Control
+                as="select"
                 name="state"
                 value={formData.state}
                 onChange={handleStateChange}
@@ -200,29 +219,30 @@ const App = () => {
               >
                 <option value="">Selecione um estado</option>
                 {estados.map((estado) => (
-                  <option key={estado.id} value={estado.sigla}>
-                    {estado.sigla} - {estado.nome}
+                  <option key={estado.sigla} value={estado.sigla}>
+                    {estado.nome}
                   </option>
                 ))}
-              </Form.Select>
+              </Form.Control>
             </Form.Group>
-            <Form.Group className="mb-3">
-              <Form.Label>Município</Form.Label>
-              <Form.Select
+            <Form.Group controlId="city">
+              <Form.Label>Cidade</Form.Label>
+              <Form.Control
+                as="select"
                 name="city"
                 value={formData.city}
                 onChange={handleInputChange}
                 required
               >
-                <option value="">Selecione um município</option>
+                <option value="">Selecione uma cidade</option>
                 {cidades.map((cidade) => (
                   <option key={cidade.id} value={cidade.nome}>
                     {cidade.nome}
                   </option>
                 ))}
-              </Form.Select>
+              </Form.Control>
             </Form.Group>
-            <Form.Group className="mb-3">
+            <Form.Group controlId="neighborhood">
               <Form.Label>Bairro</Form.Label>
               <Form.Control
                 type="text"
@@ -232,7 +252,7 @@ const App = () => {
                 required
               />
             </Form.Group>
-            <Form.Group className="mb-3">
+            <Form.Group controlId="registrationDate">
               <Form.Label>Data de Cadastro</Form.Label>
               <Form.Control
                 type="date"
@@ -243,21 +263,22 @@ const App = () => {
               />
             </Form.Group>
             <Button variant="primary" type="submit">
-              {editIndex === -1 ? "Cadastrar" : "Salvar"}
+              {editIndex === -1 ? "Adicionar" : "Editar"}
             </Button>
           </Form>
         </Modal.Body>
       </Modal>
-      <h2>Usuários Cadastrados</h2>
+
       <Table striped bordered hover>
         <thead>
           <tr>
             <th>Nome</th>
-            <th>Email</th>
+            <th>E-mail</th>
             <th>Telefone</th>
+            <th>CEP</th>
             <th>Endereço</th>
             <th>Estado</th>
-            <th>Município</th>
+            <th>Cidade</th>
             <th>Bairro</th>
             <th>Data de Cadastro</th>
             <th>Ações</th>
@@ -269,6 +290,7 @@ const App = () => {
               <td>{user.name}</td>
               <td>{user.email}</td>
               <td>{user.phone}</td>
+              <td>{user.cep}</td>
               <td>{user.address}</td>
               <td>{user.state}</td>
               <td>{user.city}</td>
@@ -297,5 +319,3 @@ const App = () => {
 };
 
 export default App;
-
-
